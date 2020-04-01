ef3941dc02de777e88fc14a23b030f4301fd9d11
Alex St.Clair <tug89508@temple.edu>
2020-03-31 23:18:25 -0400

    Implemented link parsing algorithm

--- urlconverter MessagesExtension/MessagesViewController.swift
+++ urlconverter MessagesExtension/MessagesViewController.swift
@@ -14,7 +14,10 @@ class MessagesViewController: MSMessagesAppViewController {
     //global variables
     @IBOutlet weak var textBox: UITextField!    //the text box in the UI
     @IBOutlet weak var enterButton: UIButton!   //the enter button in the UI
-    var origLink = "";
+    var origLink = "";                          //the original link inputted
+    let platforms:[String: Int] = ["Spotify": 0,
+                     "Apple Music": 1];         //the map of platforms
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -91,13 +94,43 @@ class MessagesViewController: MSMessagesAppViewController {
      - Parameter sender: the button being attached
      */
     @IBAction func onClick(_ sender: UIButton) {
-        if(!(textBox.text?.elementsEqual("Paste link here"))!){
-            origLink = textBox.text!;
-            //origLink now contains the link that the user entered
-            print(origLink);
+        origLink = textBox.text!;
+        //origLink now contains the link that the user entered
+        print("User entered \(origLink)");
+    
+        /*  we need to pass this link to a worker function that will
+            determine which platform the link is from */
+        let fromService = parseLink(link: origLink);
+
+        if(fromService == -1){
+            //Could not find platform, alert the user
+            presentErrorPopup();
+        }
+    }
+    
+    /**
+     Function that parses a link and determines which platform it is from
+     - Parameter link: the link being sent to the function
+     */
+    func parseLink(link: String) -> Int{
+        if(link.contains("open.spotify.com")){
+            return self.platforms["Spotify"]!;
+        }else if(link.contains("music.apple.com")){
+            return self.platforms["Apple Music"]!;
         }
         else{
-            print("No link has been entered.");
+            return -1;
         }
+    }
+    
+    /**
+     Creates an alert popup and presents it to the user
+     */
+    func presentErrorPopup() -> Void{
+        let alert = UIAlertController(title: "Error", message: "The platform for this link was not found. ", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertAction.Style.cancel))
+        
+        self.present(alert, animated: true, completion: nil)
     }
 }
