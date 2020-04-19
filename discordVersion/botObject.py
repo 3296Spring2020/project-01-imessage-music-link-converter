8ca5f3cf5a2e27517df56ef25e40025c7e09b287
Juvenal <jarellanobooking@gmail.com>
2020-04-19 19:51:54 -0400

    spotify search for song meta data

--- discordVersion/botObject.py
+++ discordVersion/botObject.py
@@ -1,6 +1,8 @@
 import re
 import discord
-import picklerick
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 class musicbot:
     musicPlatformDirectory = {'youtube' : 'Youtube', 'open.spotify' : 'Spotify', 'soundcloud': 'Soundcloud'}
     helpmessage = 'Music Buddy currently supports the following platforms: '
@@ -38,19 +40,31 @@ class release_interface:
         pass
 
 class search_factory:
-    def inp (self, type):
-        if type == 'open.spotify':
+    def create_search (self, type):
+        if type == 'Spotify':
             return spotify_search()
-        if type == 'youtube':
+        if type == 'Youtube':
             return  
+        else:
+            return 'dairy'
 
 
             
 class spotify_search(search_interface):
     def __init__(self):
-        pass
-    def search(self):
-        return 'spotify'
+        self.client_ID = 'c61123f4ed8945578c7cdb250f75b4b5'
+        self.client_secret_key = 'd9f9ffd47a574c9ba9aba01b5662f5d0'
+        self.credentials_manager = SpotifyClientCredentials(client_id = self.client_ID, client_secret = self.client_secret_key)
+        self.spotify_object =  spotipy.Spotify(client_credentials_manager = self.credentials_manager)
+    def search(self, url ):
+        # https://open.spotify.com/album/4eLPsYPBmXABThSJ821sqY?highlight=spotify:track:7KXjTSCq5nL1LoYtL7XAwS
+        remove_site = re.sub('https://open.spotify.com/album/','', url)
+        seperate = re.sub('highlight=spotify:track:','/', remove_site)
+        song_ID = seperate.split('/')
+        song_ID[0] = song_ID[0][:-1] 
+        song = self.spotify_object.track(song_ID[1])
+        meta_data = {"artist" : song['artists'][0]['name'] , 'album' :  song['album']['name'] , "song": song['name'] }
+        return meta_data2
     def user_music():
         pass
 
--- discordVersion/main.py
+++ discordVersion/main.py
@@ -1,30 +1,35 @@
 #from botObject import musicbot
 import botObject
 import discord
-import picklerick
+#import picklerick
 import re
-'''
+
 client = discord.Client()
 @client.event
 async def on_message(message):
     if message.content.startswith('help buddy'):
-        pass
+        #pass
         # for key in self.musicPlatformDirectory:
         #     self.helpmessage += self.musicPlatformDirectory[key]
-        # await message.channel.send(self.helpmessage)
+        mb = botObject.musicbot('')
+        await message.channel.send(mb.helpmessage)
     if message.content.startswith('go buddy'):
         tokens = (message.content).split(' ')
         url = tokens[2]
-        mb = musicbot('Njk2NDM4MzEzMjE2NjM5MDA3.XpYMlA.BP7IY80x-EbPfdgLjSNVWQ3Gdgc')
+        mb = botObject.musicbot('Njk2NDM4MzEzMjE2NjM5MDA3.XpYMlA.BP7IY80x-EbPfdgLjSNVWQ3Gdgc')
         platform = mb.findGivenPlatform(url)
-        await message.channel.send(platform)
+        search_creation_object = botObject.search_factory()
+        search_object = search_creation_object.create_search(platform)
+        post_search = search_object.search(url)
+        await message.channel.send(post_search)
 client.run("Njk2NDM4MzEzMjE2NjM5MDA3.XpYMlA.BP7IY80x-EbPfdgLjSNVWQ3Gdgc")
 
 
 
 '''
 
-a = botObject.factory()
-c = a.inp('open.spotify')
-b = c.search()
+a = botObject.search_factory()
+c = a.create_search('open.spotify')
+b = c.search('url')
 print(b)
+'''
